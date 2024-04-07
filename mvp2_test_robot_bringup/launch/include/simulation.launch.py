f8490303d09706b809e87591c3303dd8bd9dcce3
mingxi <mzhou@uri.edu>
2024-04-06 22:01:02 -0400

    commented out some rejection threshold in robot localization. this is causing euler angle freeze

--- mvp2_test_robot_bringup/launch/include/simulation.launch.py
+++ mvp2_test_robot_bringup/launch/include/simulation.launch.py
@@ -18,7 +18,7 @@ def generate_launch_description():
     scenario_desc = os.path.join(world_of_stonefish_dir, 'world', sim_world)
     simulation_rate = "100"
     window_res_x = "1600"
-    window_res_y = "800"
+    window_res_y = "1200"
     rendering_quality ="high"
 
     robot_param_path = os.path.join(
