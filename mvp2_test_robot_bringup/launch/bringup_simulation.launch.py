bd886c873df98cc0cce8913ca553b415f40dbca3
mzhouURI <mzhou@uri.edu>
2025-07-17 22:30:20 -0400

    added surfacing and wpt stuff

--- mvp2_test_robot_bringup/launch/bringup_simulation.launch.py
+++ mvp2_test_robot_bringup/launch/bringup_simulation.launch.py
@@ -8,6 +8,7 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, PythonExpression
 import time
 
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 
 
 
@@ -57,6 +58,17 @@ def generate_launch_description():
         launch_arguments = {'arg_robot_name': arg_robot_name}.items()  
     )
 
+    foxglove = IncludeLaunchDescription(
+        XMLLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('foxglove_bridge'),
+                'launch/foxglove_bridge_launch.xml')),
+        launch_arguments={
+            'namespace': arg_robot_name
+        }.items()
+    )
+
+
 
     return LaunchDescription([
         simulation,
@@ -64,6 +76,7 @@ def generate_launch_description():
         description,
         mvp_control,
         mvp_mission,
+        foxglove
         # usbl_test
         # joy
     ])
