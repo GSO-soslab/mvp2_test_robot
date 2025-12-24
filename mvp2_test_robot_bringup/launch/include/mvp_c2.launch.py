2fafb0dfd5b669790e767a5da642e612dd52382a
mzhouURI <mzhou@uri.edu>
2025-12-23 21:51:40 -0500

    update reporter

--- mvp2_test_robot_bringup/launch/include/mvp_c2.launch.py
+++ mvp2_test_robot_bringup/launch/include/mvp_c2.launch.py
@@ -27,20 +27,21 @@ def generate_launch_description():
         #         ('dccl_msg_rx', 'mvp_c2/dccl_msg_rx'),
         #     ]
         # ),
+        
         #udp
-        # Node(
-        #     package = 'mvp_c2',
-        #     namespace = robot_name,
-        #     executable='mvp_c2_udp_comm',
-        #     name = 'reporter_c2_udp_comm',
-        #     output='screen',
-        #     prefix=['stdbuf -o L'],
-        #     parameters=[reporter_setting_file],
-        #     remappings=[
-        #         ('dccl_msg_tx', 'mvp_c2/traffic_control/dccl_msg_controlled_tx'),
-        #         ('dccl_msg_rx', 'mvp_c2/traffic_control/dccl_msg_rx'),
-        #     ]
-        # ),
+        Node(
+            package = 'mvp_c2',
+            namespace = robot_name,
+            executable='mvp_c2_udp_comm',
+            name = 'reporter_c2_udp_comm',
+            output='screen',
+            prefix=['stdbuf -o L'],
+            parameters=[reporter_setting_file],
+            remappings=[
+                ('dccl_msg_tx', 'mvp_c2/traffic_control/dccl_msg_controlled_tx'),
+                ('dccl_msg_rx', 'mvp_c2/traffic_control/dccl_msg_rx'),
+            ]
+        ),
 
         #DCCL reporter node
         Node(
